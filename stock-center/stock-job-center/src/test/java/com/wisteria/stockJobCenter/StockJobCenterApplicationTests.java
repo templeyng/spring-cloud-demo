89e2a650b0e7d235fc4de6b1c82bdc1afadc2611
Yang <templeyng_cy@163.com>
2022-03-31 18:08:17 +0800

    FTP测试程序

--- stock-center/stock-job-center/src/test/java/com/wisteria/stockJobCenter/StockJobCenterApplicationTests.java
+++ stock-center/stock-job-center/src/test/java/com/wisteria/stockJobCenter/StockJobCenterApplicationTests.java
@@ -1,9 +1,7 @@
 package com.wisteria.stockJobCenter;
 
-import com.wisteria.common.utils.SaleRedisTemplate;
-import com.wisteria.stockJobCenter.service.SkuStockService;
+import com.wisteria.stockJobCenter.util.FavFTPUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
@@ -13,13 +11,21 @@ import org.springframework.context.annotation.ComponentScans;
         @ComponentScan(value = "com.wisteria.*")
 })
 class StockJobCenterApplicationTests {
-    @Autowired
-    private SkuStockService skuStockService;
-
-    @Autowired
-    private SaleRedisTemplate saleRedisTemplate;
 
     @Test
     void contextLoads() {
+        String hostname = "116.6.111.178";
+        int port = 8021;
+        String username = "amazonread";
+        String password = "L9kgpztnEj";
+        String pathname = "2017manualFile/jinnitie";
+        String filename = "2021-11-01_jinnitie_MonthlyTransaction_FR.csv";
+//        String originfilename = "C:\\Users\\Downloads\\Downloads.rar";
+//        FavFTPUtil.uploadFileFromProduction(hostname, port, username, password, pathname, filename, originfilename);
+
+        String localpath = "D:/";
+
+        FavFTPUtil.downloadFile(hostname, port, username, password, pathname, filename, localpath);
+
     }
 }
